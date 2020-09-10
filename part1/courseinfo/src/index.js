63d20dbadad4e2b372a33ea4b10850306e5d9c76
kcc343 <kcc343@uw.edu>
2020-09-10 14:47:04 -0700

    modified content function based on exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,33 +3,35 @@ import ReactDOM from 'react-dom';
 
 const Header = (obj) => {
   return (
-    <>
+    <div>
       <h1>{obj.course}</h1>
-    </>
+    </div>
   )
 }
 
 const Content = (obj) => {
   return (
-    <>
-      <p>
-        {obj.part1} {obj.exercises1}
-      </p>
-      <p>
-        {obj.part2} {obj.exercises2}
-      </p>
-      <p>
-        {obj.part3} {obj.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={obj.part1} number={obj.exercises1}/>
+      <Part part={obj.part2} number={obj.exercises2} />
+      <Part part={obj.part3} number={obj.exercises3} />
+    </div>
+  )
+}
+
+const Part = (obj) => {
+  return (
+    <div>
+      <p>{obj.part} {obj.number}</p>
+    </div>
   )
 }
 
 const Total = (obj) => {
   return (
-    <>
+    <div>
       <p>Number of exercises {obj.total}</p>
-    </>
+    </div>
   )
 }
 
