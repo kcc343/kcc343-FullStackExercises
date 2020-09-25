65e24e9f63ceb9113c98f87ca84988cdc51c3f9f
kcc343 <kcc343@uw.edu>
2020-09-24 22:35:27 -0700

    update to step 4 for adding name filtering

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, {useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [show, setShow] = useState('')
 
   const addData = (event) => {
     event.preventDefault()
@@ -32,9 +36,18 @@ const App = () => {
     setNewNum(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setShow(event.target.value)
+  }
+  let filterPersons = persons.filter(person => person.name.toLowerCase().startsWith(show))
+
   return (
     <div className="App">
       <h2>Phonebook</h2>
+      <div>
+        <input onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addData}>
         <div>
           name: <input 
@@ -50,7 +63,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {filterPersons.map(person => 
           <div key={person.name}>{person.name} {person.number}</div>
       )}
     </div>
