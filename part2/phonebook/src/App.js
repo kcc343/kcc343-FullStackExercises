2c5b5a6ad06a6172076a2f99d30dab6177b2f232
kcc343 <kcc343@uw.edu>
2020-10-13 22:55:01 -0700

    update to step6 to get data from axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Filter from './components/filter'
 import PersonForm from './components/form'
 import Person from './components/person'
+import axios from 'axios'
 
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+const App = (data) => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [show, setShow] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/Persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addData = (event) => {
     event.preventDefault()
