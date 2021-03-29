27ef39273760c659db6a3020921779aac063a17e
kcc343 <kcc343@uw.edu>
2021-03-29 09:38:29 -0700

    ex 3.19 added validation to verify name of person is unique

--- part3/phonebook_database/index.js
+++ part3/phonebook_database/index.js
@@ -12,6 +12,7 @@ morgan.token('data', function(req, res) {
   return JSON.stringify(req.body);
 });
 
+
 const Person = require('./models/person');
 
 app.get('/api/persons', (request, response) => {
@@ -48,13 +49,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body.name) {
     return response.status(400).json({ 
       error: 'name missing' 
     })
-  } else if (!body.number) {
+  } else if (!body.phone) {
     return response.status(400).json({ 
       error: 'number missing' 
     })
@@ -66,8 +67,9 @@ app.post('/api/persons', (request, response) => {
   })
 
   person.save().then(savedPerson => {
-    response.json(savedPerson)
+    response.json(savedPerson.toJSON())
   })
+  .catch(error => next(error))
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -89,6 +91,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message })
   }
 
   next(error)
