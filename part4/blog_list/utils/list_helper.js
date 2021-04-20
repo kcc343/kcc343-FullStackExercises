809f07c8b215e511e7f5b0f20a186d7d37307d07
kcc343 <kcc343@uw.edu>
2021-04-20 16:40:01 -0700

    ex 4.5 added tests to get the most liked post

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -10,7 +10,22 @@ const totalLikes = (blogs) => {
 	return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	if (blogs.length == 0) {
+		return {}
+	}
+	let mostLiked = blogs[0]
+	
+	for (let i = 1; i < blogs.length; i++) {
+		if (mostLiked.likes < blogs[i].likes) {
+			mostLiked = blogs[i]
+		}
+	}
+	return mostLiked
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
