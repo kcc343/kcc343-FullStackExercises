088b6c2670ac85d9b2826d58728f71c5725a4b59
kcc343 <kcc343@uw.edu>
2021-05-13 23:03:37 -0700

    ex 4.9 write test to verify unique property is named id for blog posts

--- part4/blog_list_tests/tests/blog_list_api.test.js
+++ part4/blog_list_tests/tests/blog_list_api.test.js
@@ -4,11 +4,19 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blogs have id property', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+  for (let blog of blogs) {
+    expect(blog.id).toBeDefined()
+  }
 })
 
 afterAll(() => {
